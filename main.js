bb24ad401e86015163a61f20e82ee560330230a3
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-12 11:41:47 +0530

    json local storage

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function saveToLocalStorage(event){
   event.preventDefault();
-  var name=event.target.name.value;
-  var email=event.target.email.value;
-  localStorage.setItem('name' , name);
-  localStorage.setItem('email', email);
+  var myObj={
+  name:event.target.name.value,
+  email:event.target.email.value
+  }
+
+  localStorage.setItem('Obj' , JSON.stringify(myObj));
+
 }
