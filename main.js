3d1fd3c05ff71a7cace26df8245c67fd7de137d9
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-12 13:40:43 +0530

    store single

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ function saveToLocalStorage(event){
   email:event.target.email.value
   }
 
-  localStorage.setItem('Obj' , JSON.stringify(myObj));
-
+  localStorage.setItem(myObj.email , JSON.stringify(myObj));
+  obj1=JSON.parse(localStorage.getItem(myObj.email));
+  //Add new li
+var ul = document.getElementById("users");
+var li = document.createElement("li");
+// create new li element
+var li=document.createElement('li');
+//Add Class
+li.className='item';
+li.appendChild(document.createTextNode("Name = " + obj1.name + " Email = " + obj1.email));
+console.log(li)
+ul.appendChild(li);
 }
+
+ 
